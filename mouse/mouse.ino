721788ac39942878ee8c92f7d430ae45c615e8fd
cuijunjie <cuijunjie@armvm.com>
2020-10-29 22:00:25 +0800

    add photo

--- mouse/mouse.ino
+++ mouse/mouse.ino
@@ -1,17 +1,15 @@
-#include <Bounce2.h>
-
 #include <Mouse.h>
 #include <Keyboard.h>
 
 const int pingRockerY = A0;
 const int pingRockerX = A1;
 
-const int pingButtonBlue = 2;
-const int pingButtonGreen = 3;
-const int pingButtonRed = 4;
+const int pingButtonBlue = 15;
+//const int pingButtonGreen = 14;
+const int pingButtonRed = 16;
 
-const int pingWheelLeft = 5;
-const int pingWheelRight = 6;
+const int pingWheelLeft = 2;
+const int pingWheelRight = 3;
 
 int responseDelay = 5;
 
@@ -22,6 +20,7 @@ int lastAxisY;
 int lastAxisX;
 int tempAxis;
 int distance;
+int distanceParam = 80;
 
 //按钮
 boolean leftButtonStatus = false;
@@ -29,18 +28,12 @@ boolean middleButtonStatus = false;
 boolean rightButtonStatus = false;
 
 //滚轮
-int tempLeftWheel;
-int tempRightWheel;
-int lastLeftWheel;
-int lastRightWheel;
-
-Bounce debouncer1 = Bounce();
-Bounce debouncer2 = Bounce();
-
+boolean A_set;
+boolean B_set;
 
 void setup() {
   pinMode(pingButtonBlue, INPUT);
-  pinMode(pingButtonGreen, INPUT);
+//  pinMode(pingButtonGreen, INPUT);
   pinMode(pingButtonRed, INPUT);
   pinMode(pingRockerY, INPUT);
   pinMode(pingRockerX, INPUT);
@@ -51,45 +44,46 @@ void setup() {
     lastAxisY = analogRead(pingRockerX);
     lastAxisX = analogRead(pingRockerY);
   }
-  debouncer1.attach(pingWheelRight);
-  debouncer2.attach(pingWheelLeft);
-  debouncer1.interval(20);
-  debouncer2.interval(20);
-  debouncer1.update();
-  debouncer2.update();
-  lastRightWheel = debouncer1.read();
-  lastLeftWheel = debouncer2.read();
+  attachInterrupt(0, wheelTurnUp, CHANGE);
+  attachInterrupt(1, wheelTurnDown, CHANGE);
   Mouse.begin();
   Keyboard.begin();
+  Serial.begin (9600);
 }
 
 void loop() {
   moveMouse();
   clickMouse();
-  rollWheel();
   delay(responseDelay);
 }
 
-void rollWheel() {
-  debouncer1.update();
-  debouncer2.update();
-  tempRightWheel = debouncer1.read();
-  tempLeftWheel = debouncer2.read();
-  if ( tempLeftWheel == lastLeftWheel & tempRightWheel != lastRightWheel) {
-    Keyboard.press(KEY_PAGE_UP);
-    delay(5);
-    Keyboard.releaseAll();
+void wheelTurnUp() {
+  if (digitalRead(pingWheelLeft) == HIGH) {
+    A_set = true;
+    if (!B_set) {
+      Keyboard.press(KEY_PAGE_UP);
+      delay(1);
+      Keyboard.releaseAll();
+    }
   }
-  if ( tempRightWheel == lastRightWheel & tempLeftWheel != lastLeftWheel ) {
-    Keyboard.press(KEY_PAGE_DOWN);
-    delay(5);
-    Keyboard.releaseAll();
+  if (digitalRead(pingWheelLeft) == LOW) {
+    A_set = false;
   }
-  lastRightWheel = tempRightWheel;
-  lastLeftWheel = tempLeftWheel;
-  Serial.println(tempRightWheel + "-" + tempLeftWheel);
 }
 
+void wheelTurnDown() {
+  if (digitalRead(pingWheelRight) == HIGH) {
+    B_set = true;
+    if (!A_set) {
+      Keyboard.press(KEY_PAGE_DOWN);
+      delay(1);
+      Keyboard.releaseAll();
+    }
+  }
+  if (digitalRead(pingWheelRight) == LOW) {
+    B_set = false;
+  }
+}
 
 void clickMouse() {
   if (digitalRead(pingButtonBlue) == LOW) {
@@ -104,14 +98,14 @@ void clickMouse() {
     }
   }
 
-  if (digitalRead(pingButtonGreen) == LOW) {
-    if (middleButtonStatus == false) {
-      Mouse.click(MOUSE_MIDDLE);
-      middleButtonStatus = true;
-    }
-  } else {
-    middleButtonStatus = false;
-  }
+//  if (digitalRead(pingButtonGreen) == LOW) {
+//    if (middleButtonStatus == false) {
+//      Mouse.click(MOUSE_MIDDLE);
+//      middleButtonStatus = true;
+//    }
+//  } else {
+//    middleButtonStatus = false;
+//  }
 
   if (digitalRead(pingButtonRed) == LOW) {
     if (rightButtonStatus == false) {
@@ -138,5 +132,5 @@ int readAxis(int thisAxis, int lastAxis) {
     distance = 0;
   }
   lastAxis = tempAxis;
-  return distance / 80 ;
+  return distance / distanceParam ;
 }
